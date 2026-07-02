653bcbeac49924f13215d033b9b3ce04299df27a
rtrevizo18 <ricardotrevizo18@gmail.com>
2026-07-02 12:05:10 -0500

    feat: add basic skeleton + possible title font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
 
 
-  return <div className="h-screen flex flex-col justify-center items-center gap-5">Count: {count}
-    <button className="h-10 w-10 bg-indigo-600" onClick={() => setCount(count + 1)}></button>
-  </div>
+  return <div className='h-screen'><header className='border h-[10%]'>header stuff<nav>nav stuff</nav></header><main>main stuff</main><footer className='border'>footer stuff</footer></div>
+
 }
 
 export default App;
